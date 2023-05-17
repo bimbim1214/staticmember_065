b6ff51caf2b2693d78789df585f41239647e4688
bimbim1214 <bimoadityapangestu@gmail.com>
2023-05-17 10:54:38 +0700

    Add project files.

---
+++ mbrstatic/mbrstatic.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+public:
+	static int nim;
+	int id;
+	string nama;
+
+	void setID();
+	void printAll();
+	mahasiswa(string pnama) :nama(pnama) { setID;  }
+};
+
+int mahasiswa::nim = 0;
+
+void 
