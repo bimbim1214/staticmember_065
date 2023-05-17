66f074ec6e0599858b9488ed3c95f01edc3a852b
bimbim1214 <bimoadityapangestu@gmail.com>
2023-05-17 19:48:10 +0700

    membuat variabel kelas angka dengan int

--- destruktor/destruktor.cpp
+++ destruktor/destruktor.cpp
@@ -11,3 +11,17 @@ public:
 	void cetakData();
 	void isiData();
 };
+
+angka::angka(int i) {
+	panjang = i;
+	arr = new int[i];
+	isiData();
+}
+
+angka::~angka() {
+	cout << endl;
+	cetakData();
+	delete[]arr;
+	cout << "alamat array sudah dilepaskan" << endl;
+}
+
