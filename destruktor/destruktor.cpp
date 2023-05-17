39c2224cb33d88aee62d35ea137c9f48157fea7b
bimbim1214 <bimoadityapangestu@gmail.com>
2023-05-17 19:46:39 +0700

    membaut class angka dengan 4 variabel

--- destruktor/destruktor.cpp
+++ destruktor/destruktor.cpp
@@ -11,38 +11,3 @@ public:
 	void cetakData();
 	void isiData();
 };
-
-angka::angka(int i) {
-	panjang = i;
-	arr = new int[i];
-	isiData();
-}
-
-angka::~angka() {
-	cout << endl;
-	cetakData();
-	delete[]arr;
-	cout << "alamat array sudah dilepaskan" << endl;
-}
-
-void angka::cetakData() {
-	for (int i = 1; i <= panjang;i++) {
-		cout << i << "=" << arr [i] << endl;
-	}
-}
-
-void angka::isiData() {
-	for (int i = 1; i <= panjang;i++) {
-		cout << i << "=";
-		cin >> arr[i];
-	}
-	cout << endl;
-}
-
-int main() {
-	angka belajarcpp(3);
-	angka* ptrbelajarcpp = new angka(5);
-	delete ptrbelajarcpp;
-
-	return 0;
-}
